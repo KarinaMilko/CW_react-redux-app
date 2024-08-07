b84a4112c09262c8304231e5d0ad04c9d7efc8d8
Karina <milkokarina13@gmail.com>
2024-08-07 22:51:01 +0300

    Add toolkit + counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,18 +1,29 @@
 import { connect } from "react-redux";
+import {
+  decrement,
+  increment,
+  setStep,
+} from "./../../store/slices/counterSlice";
 
 function Counter({ count, step, dispatch }) {
   const dec = () => {
-    const action = { type: "decrement" };
+    // const action = { type: "decrement" };
+    const action = decrement();
     dispatch(action); // ніби виклик reducer
   };
 
   const inc = () => {
-    const action = { type: "increment" };
+    // const action = { type: "increment" };
+    const action = increment();
     dispatch(action);
   };
 
-  const setStep = ({ target: { value } }) => {
-    const action = { type: "setStep", payload: Number(value) };
+  // const setStep = ({ target: { value } }) => {
+  //   const action = { type: "setStep", payload: Number(value) };
+  //   dispatch(action);
+  // };
+  const changeStep = ({ target: { value } }) => {
+    const action = setStep(Number(value));
     dispatch(action);
   };
 
@@ -22,14 +33,17 @@ function Counter({ count, step, dispatch }) {
       {count}
       <button onClick={inc}>+</button>
       <div>
-        <input type="number" value={step} onChange={setStep} />
+        {/* <input type="number" value={step} onChange={setStep} />
+         */}
+        <input type="number" value={step} onChange={changeStep} />
       </div>
     </div>
   );
 }
 
 function mapStateToProps(state) {
-  return state;
+  // return state;
+  return state.counter;
 } // { count: 0, step: 1} => Counter ({count, state})
 
 // створює HOC, який прокине dispatch в пропси
---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const SLICE_NAME = "counter";
+
+const initialState = { count: 0, step: 1 };
+
+const counterSlice = createSlice({
+  initialState,
+  name: SLICE_NAME,
+  reducers: {
+    // method(state, action){}
+    decrement(state) {
+      const { count, step } = state;
+      state.count = count - step;
+    },
+    increment(state) {
+      const { count, step } = state;
+      state.count = count + step;
+    },
+    setStep(state, { payload }) {
+      state.step = payload;
+    },
+  },
+});
+// console.log(counterSlice);
+
+const { reducer, actions } = counterSlice;
+
+export const { decrement, increment, setStep } = actions; // actionCreators
+
+export default reducer;
+// decrement() => {type: "counter/decrement"}
+// setStep(v) => {type: 'counter/setStep', payload:v}
