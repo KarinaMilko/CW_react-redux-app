ef5d62f4627b9927b11d38b0f01baa3ea6ebc7a0
Karina <milkokarina13@gmail.com>
2024-08-09 15:47:29 +0300

    Add mapDispatchToProps

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -5,27 +5,8 @@ import {
   setStep,
 } from "./../../store/slices/counterSlice";
 
-function Counter({ count, step, dispatch }) {
-  const dec = () => {
-    // const action = { type: "decrement" };
-    const action = decrement();
-    dispatch(action); // ніби виклик reducer
-  };
-
-  const inc = () => {
-    // const action = { type: "increment" };
-    const action = increment();
-    dispatch(action);
-  };
-
-  // const setStep = ({ target: { value } }) => {
-  //   const action = { type: "setStep", payload: Number(value) };
-  //   dispatch(action);
-  // };
-  const changeStep = ({ target: { value } }) => {
-    const action = setStep(Number(value));
-    dispatch(action);
-  };
+function Counter({ count, step, dec, inc, updateStep }) {
+  const changeStep = ({ target: { value } }) => updateStep(value);
 
   return (
     <div>
@@ -33,23 +14,28 @@ function Counter({ count, step, dispatch }) {
       {count}
       <button onClick={inc}>+</button>
       <div>
-        {/* <input type="number" value={step} onChange={setStep} />
-         */}
         <input type="number" value={step} onChange={changeStep} />
       </div>
     </div>
   );
 }
-
 function mapStateToProps(state) {
-  // return state;
   return state.counter;
 } // { count: 0, step: 1} => Counter ({count, state})
 
+function mapDispatchToProps(dispatch) {
+  return {
+    dec: () => dispatch(decrement()),
+    inc: () => dispatch(increment()),
+    updateStep: (value) => dispatch(setStep(Number(value))),
+  };
+}
+
 // створює HOC, який прокине dispatch в пропси
 // перший параметр приймає функцію, щоб проникути в пропси стан
-const withAccessToStore = connect(mapStateToProps); // HOC
+
+const withAccessToStore = connect(mapStateToProps, mapDispatchToProps); // HOC
 
 export default withAccessToStore(Counter);
 
-// HOC — function(Component)=>Component
+// HOC - function(Component)=>Component
